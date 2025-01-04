526f5538a1d309b3dd5165c6fa8b8b71c4dca22b
Yatinghsu000627 <997726tina@gmail.com>
2025-01-04 16:21:31 +0800

    calculate single leg and for only one cycle

--- src/corgi_gait_generate/wlw_gen/include/wlw.hpp
+++ src/corgi_gait_generate/wlw_gen/include/wlw.hpp
@@ -3,8 +3,25 @@
 
 #include <chrono>
 #include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <iomanip>
+#include <algorithm>
 #include <Eigen/Dense>
 #include <Eigen/Geometry>
 #include "leg_model.hpp"
 
+using namespace std;
+const double PI = M_PI;
+double walking_distance, step_length, velocity, tilt_angle_deg, tilt_angle_rad;
+vector<pair<double, double>> terrain_positions;
+
+double deg2rad(double degrees);
+double rad2deg(double radians);
+void genTerrainPosition(double walking_distance, double tilt_angle_rad, vector<pair<double, double>> &terrain_positions);
+void setting(double &walking_distance, double &step_length, double &velocity, double &tilt_angle_deg, double &tilt_angle_rad);
+void setting_calculation(double walking_distance, double step_length, double tilt_angle_rad, const vector<pair<double, double>> &terrain_positions, bool output_details);
+void findMinMaxHeight(const vector<pair<double, double>> &terrain_positions);
 #endif
