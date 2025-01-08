1267f2f34f7e6b70ed22e7a308149da554270165
hiho817 <kenny0817123@gmail.com>
2025-01-08 20:33:02 +0800

    implement Encoder class for encoder state management

--- src/corgi_odometry/src/corgi_odometry/corgi_odometry.cpp
+++ src/corgi_odometry/src/corgi_odometry/corgi_odometry.cpp
@@ -4,7 +4,7 @@
 #include <vector>
 #include "ros/ros.h"
 
-#include "corgi_msgs/MotorCmdStamped.h"
+#include "corgi_msgs/MotorStateStamped.h"
 #include "corgi_msgs/TriggerStamped.h"
 #include "KLD_estimation/InformationFilter.hpp"
 #include "KLD_estimation/csv_reader.hpp"
@@ -18,14 +18,64 @@ const float MOTOR_OFFSET[3] = {0.222, 0.193, 0};
 const float WHEEL_RADIUS = 0.1;
 const float WHEEL_WIDTH = 0.012;
 
+//Classes
+class Encoder{
+    public:
+        Encoder(corgi_msgs::MotorState* m, corgi_msgs::IMUStamped* i, float dt)//FIXME: undetermined IMU msg
+        :   module(m), 
+            imu(i),
+            theta(m->theta),
+            beta(m->beta),
+            theta_prev(m->theta),
+            beta_prev(m->beta),
+            w_y(i->w_y) //FIXME: undetermined msg
+        {
+            updateState();
+        }
+            
+        void updateState(float dt) {
+            // TODO: update
+            beta_d = 
+            theta_d = 
+            theta_prev = theta;
+            beta_prev  = beta;
+            theta = module->theta;
+            beta  = module->beta;
+            state << theta, beta, beta_d, w_y, theta_d;
+        }
+        //const reference, prevent modified
+        const Eigen::Matrix<float, 5, 1>& getState() const {
+            return state;
+        }
+    private:
+        corgi_msgs::MotorState* module;
+        corgi_msgs::IMUStamped* imu;
+        float theta;
+        float beta;
+        float theta_prev;
+        float beta_prev;
+        float theta_d;
+        float beta_d;
+        float w_y;
+        Eigen::Vector<float, 5> state
+}
+
 // Variables
 bool trigger = false;
 float dt = 1 / (float)SAMPLE_RATE;
+
+corgi_msgs::MotorStateStamped motor_state;
 
 // Callbacks
 void trigger_cb(const corgi_msgs::TriggerStamped msg){
     trigger = msg.enable;
 }
+
+void motor_state_cb(const corgi_msgs::MotorStateStamped state){
+    motor_state = state;
+
+}
+// Eigen::Vector<float, 5> encoder_lf(df.iloc("lf.theta", start_index), df.iloc("lf.beta", start_index), df.iloc("lf.beta_d", start_index), df.iloc("w.y", start_index), df.iloc("lf.theta_d", start_index));
 
 int main(int argc, char **argv) {
     ros::init(argc, argv, "corgi_odometry");
@@ -51,6 +101,7 @@ int main(int argc, char **argv) {
     };*/
     // ROS Subscribers
     ros::Subscriber trigger_sub = nh.subscribe<corgi_msgs::TriggerStamped>("trigger", SAMPLE_RATE, trigger_cb);
+    ros::Subscriber motor_state_sub = nh.subscribe<corgi_msgs::MotorStateStamped>("motor/state", SAMPLE_RATE, motor_state_cb);
     // TODO: Add IMU(ax,ay,az),IMU(wx,wy,wz),IMU(quaternion),leg encoder(theta,beta,(beta_d,theta_d))
 
     Eigen::initParallel();
@@ -68,11 +119,15 @@ int main(int argc, char **argv) {
     Leg lh_leg(Eigen::Vector3f(-MOTOR_OFFSET[0],  MOTOR_OFFSET[1], MOTOR_OFFSET[2]), WHEEL_RADIUS, WHEEL_WIDTH);
     
     //Dynamic predictor
-    
-    DP lf(J + 1, lf_leg, encoder_lf, 0, &u);
-    DP rf(J + 1, rf_leg, encoder_rf, 0, &u);
-    DP rh(J + 1, rh_leg, encoder_rh, 0, &u);
-    DP lh(J + 1, lh_leg, encoder_lh, 0, &u);
+    Encoder encoder_lf;
+    Encoder encoder_rf;
+    Encoder encoder_rh;
+    Encoder encoder_lh;
+    Eigen::Vector<float, 5> encoder_lf();
+    DP lf(J + 1, lf_leg, encoder_lf.state, 0, &u);
+    DP rf(J + 1, rf_leg, encoder_rf.state, 0, &u);
+    DP rh(J + 1, rh_leg, encoder_rh.state, 0, &u);
+    DP lh(J + 1, lh_leg, encoder_lh.state, 0, &u);
 
     while (ros::ok()){
         ros::spinOnce();
