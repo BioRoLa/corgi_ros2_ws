ff81216a14c5739e57f0d29e62c2e4b6e574d43b
kyle1548 <kyle9296@gmail.com>
2025-01-22 23:42:36 +0800

    add "direction" in SwingProfile

--- src/corgi_walk/src/walk_gait.cpp
+++ src/corgi_walk/src/walk_gait.cpp
@@ -91,10 +91,10 @@ std::array<std::array<double, 4>, 2> WalkGait::step() {
         if (swing_phase[i] == 0) { // Stance phase
             result_eta = leg_model.move(theta[i], beta[i], {next_hip[i][0]-hip[i][0], next_hip[i][1]-hip[i][1]});
         } else { // Swing phase
-            if (duty[i] < 0.0) {    // direction == -1
-                swing_phase_ratio = - duty[i] / swing_time;
-            } else {    // duty[i] > (1 - swing_time) -> direction == 1
+            if ( sp[i].getDirection()==1 ) {    // direction == 1
                 swing_phase_ratio = (duty[i] - (1 - swing_time)) / swing_time;
+            } else {    // direction == -1
+                swing_phase_ratio = (1.0 - duty[i]) / swing_time;
             }//end if else
             curve_point_temp = sp[i].getFootendPoint(swing_phase_ratio);
             double curve_point[2] = {curve_point_temp[0] - next_hip[i][0], curve_point_temp[1] - next_hip[i][1]};
@@ -104,7 +104,7 @@ std::array<std::array<double, 4>, 2> WalkGait::step() {
         beta[i] = result_eta[1];
 
         duty[i] += incre_duty;
-        if (duty[i] <=0){
+        if (duty[i] < 0){
             duty[i] += 1.0;
         }//end if
         if ((duty[i] > (1 - swing_time)) && swing_phase[i] == 0) {
@@ -133,7 +133,7 @@ std::array<std::array<double, 4>, 2> WalkGait::step() {
             } else if (current_rim == 1) {  // U_l
                 p_td = {foothold[i][0] + leg_model.G[0]-leg_model.U_l[0], foothold[i][1] + leg_model.G[1]-leg_model.U_l[1] + leg_model.radius};
             }//end if else
-            sp[i] = SwingProfile(p_lo, p_td, step_height);
+            sp[i] = SwingProfile(p_lo, p_td, step_height, direction);
             if ( ((direction == 1) && (i==2 || i==3)) || ((direction == -1) && (i==0 || i==1)) ) {
                 step_length = new_step_length;
             }//end if 
