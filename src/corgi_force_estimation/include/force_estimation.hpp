e7e490fcccf2135855792d99ffca088a5b0edf28
Eason <yisyuanshen@gmail.com>
2025-01-25 15:22:28 +0800

    Migrate force_estimation to a new package

--- src/corgi_force_estimation/include/force_estimation.hpp
+++ src/corgi_force_estimation/include/force_estimation.hpp
@@ -1,27 +1,29 @@
-#ifndef FORCE_ESTIMATE_HPP
-#define FORCE_ESTIMATE_HPP
+#ifndef FORCE_ESTIMATION_HPP
+#define FORCE_ESTIMATION_HPP
 
 #include <iostream>
 #include <Eigen/Dense>
 #include <Eigen/Geometry>
 
 #include "leg_model.hpp"
+#include "fitted_coefficient.hpp"
 
 #include "ros/ros.h"
 #include "corgi_msgs/MotorStateStamped.h"
 #include "corgi_msgs/ForceStateStamped.h"
 
-Eigen::MatrixXd H_l_poly(2, 8);
-Eigen::MatrixXd F_l_poly(2, 8);
-Eigen::MatrixXd F_r_poly(2, 8);
-Eigen::MatrixXd U_l_poly(2, 8);
-Eigen::MatrixXd U_r_poly(2, 8);
-Eigen::MatrixXd L_l_poly(2, 8);
-Eigen::MatrixXd L_r_poly(2, 8);
-Eigen::MatrixXd G_poly(2, 8);
-Eigen::MatrixXd P_poly(2, 8);
+bool sim = true;
+LegModel legmodel(sim);
 
-void motor_state_cb(const corgi_msgs::MotorStateStamped state);
+Eigen::MatrixXd H_l_coef(2, 8);
+Eigen::MatrixXd F_l_coef(2, 8);
+Eigen::MatrixXd F_r_coef(2, 8);
+Eigen::MatrixXd U_l_coef(2, 8);
+Eigen::MatrixXd U_r_coef(2, 8);
+Eigen::MatrixXd L_l_coef(2, 8);
+Eigen::MatrixXd L_r_coef(2, 8);
+Eigen::MatrixXd G_coef(2, 8);
+
 Eigen::MatrixXd calculate_P_poly(int rim, double alpha);
 Eigen::MatrixXd calculate_jacobian(Eigen::MatrixXd P_theta, Eigen::MatrixXd P_theta_deriv, double beta);
 Eigen::MatrixXd estimate_force(double theta, double beta, double torque_r, double torque_l);
--- src/corgi_force_estimation/src/force_estimation.cpp
+++ src/corgi_force_estimation/src/force_estimation.cpp
@@ -1,11 +1,8 @@
 #include "force_estimation.hpp"
 
 
-LegModel legmodel(true);
-
 corgi_msgs::MotorStateStamped motor_state;
 corgi_msgs::ForceStateStamped force_state;
-
 
 void motor_state_cb(const corgi_msgs::MotorStateStamped state){
     motor_state = state;
@@ -15,27 +12,32 @@ Eigen::MatrixXd calculate_P_poly(int rim, double alpha){
     Eigen::Rotation2D<double> rotation(alpha);
     Eigen::Matrix2d rot_alpha = rotation.toRotationMatrix();
 
-    H_l_poly <<  0.02986810945369848,  -0.10150955831419921,   0.00033837074403774705, 0.006765373774739306,  0.007615792562309228,  -0.0024121994732995045, -6.520446163389534e-05,   5.435366353719506e-05,
-                 0.0984219968319075,    0.020210699222679533, -0.04976557612069304,   -0.0023520295662631807, 0.0029910867610955538,  0.0009163508036696394, -0.00032806661645546487,  1.823661837557865e-05;
-    U_l_poly <<  0.009669527527254635, -0.03326882772877039,   0.0014183676837420903,  0.002963308891146737,  0.0008700406282839998,  0.0007517214838673226, -0.0003701278840337812,   2.5056958461185766e-05,
-                -0.0006690023490031166, 0.014773023018696044, -0.04975560872784549,    0.029792034672425482, -0.019784732744835352,   0.004526695454915333,   0.0003729635468404478,  -0.00016159426009107307;
-    F_l_poly << -0.07922197995394527,   0.006429966727950573,  0.002120211801039433,   0.025143170481931387, -0.02091333510279192,    0.005551135130921033,  -2.9108620785113164e-05, -0.00013153304571884635,
-                -0.04889045741031588,  -0.04099768033007372,  -0.050576527465387176,   0.05336639156632884,  -0.029257201539865652,   0.004423676195682945,   0.0010571619431373274,  -0.00025474474248363046;
-    L_l_poly << -0.006205715410243533,  0.005373735412447777,  0.06028316700203501,   -0.025480735039307013, -0.00855485481636677,    0.008709592938898975,  -0.0021348251734653874,   0.00015989475249695854,
-                 0.020478449370300727, -0.04889887701569285,  -0.08046609883658265,    0.04415062837261041,  -0.0077196354531666005, -0.004295629132118317,   0.0020770723033178957,  -0.00021893555992257824;
-    G_poly   <<  0,                     0,                     0,                      0,                     0,                      0,                      0,                       0,
-                -0.08004472811678946,  -0.04301096555457295,  -0.10580886132752444,    0.0888545682810313,   -0.031030861225472762,  -0.0011104867548842852,  0.0030345590247493667,  -0.00046519990417785516;
+    for (int i=0; i<8; i++){
+        H_l_coef(0, i) = H_x_coef[i];
+        H_l_coef(1, i) = H_y_coef[i];
+        U_l_coef(0, i) = U_x_coef[i];
+        U_l_coef(1, i) = U_y_coef[i];
+        F_l_coef(0, i) = F_x_coef[i];
+        F_l_coef(1, i) = F_y_coef[i];
+        G_coef(0, i) = 0;
+        G_coef(1, i) = G_y_coef[i];
+    }
 
-    L_r_poly << L_l_poly * (-1, 1);
-    F_r_poly << F_l_poly * (-1, 1);
-    U_r_poly << U_l_poly * (-1, 1);
+    L_r_coef = L_l_coef * (-1, 1);
+    F_r_coef = F_l_coef * (-1, 1);
+    U_r_coef = U_l_coef * (-1, 1);
 
-    if      (rim == 1) P_poly = rot_alpha * (H_l_poly-U_l_poly) / legmodel.R * legmodel.radius + U_l_poly;
-    else if (rim == 2) P_poly = rot_alpha * (F_l_poly-L_l_poly) / legmodel.R * legmodel.radius + L_l_poly;
-    else if (rim == 3) P_poly = G_poly / legmodel.R * legmodel.radius;
-    else if (rim == 4) P_poly = rot_alpha * (G_poly-L_r_poly)   / legmodel.R * legmodel.radius + L_r_poly;
-    else if (rim == 5) P_poly = rot_alpha * (F_r_poly-U_r_poly) / legmodel.R * legmodel.radius + U_r_poly;
-    else P_poly << 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0;
+
+    Eigen::MatrixXd P_poly(2, 8);
+
+    double scaled_radius = legmodel.radius / legmodel.R;
+
+    if      (rim == 1) P_poly = rot_alpha * (H_l_coef-U_l_coef) * scaled_radius + U_l_coef;
+    else if (rim == 2) P_poly = rot_alpha * (F_l_coef-L_l_coef) * scaled_radius + L_l_coef;
+    else if (rim == 3) P_poly = G_coef * scaled_radius;
+    else if (rim == 4) P_poly = rot_alpha * (G_coef-L_r_coef) * scaled_radius + L_r_coef;
+    else if (rim == 5) P_poly = rot_alpha * (F_r_coef-U_r_coef) * scaled_radius + U_r_coef;
+    else P_poly = Eigen::MatrixXd::Zero(2, 8);
     
     return P_poly;
 }
@@ -102,7 +104,6 @@ int main(int argc, char **argv) {
     ros::Publisher force_state_pub = nh.advertise<corgi_msgs::ForceStateStamped>("force/state", 1000);
     ros::Rate rate(1000);
 
-
     std::vector<corgi_msgs::MotorState*> motor_state_modules = {
         &motor_state.module_a,
         &motor_state.module_b,
@@ -117,18 +118,15 @@ int main(int argc, char **argv) {
         &force_state.module_d
     };
 
-
     while (ros::ok()) {
         ros::spinOnce();
 
         for (int i=0; i<4; i++){
-            Eigen::MatrixXd force_est = estimate_force(motor_state_modules[i]->theta, motor_state_modules[i]->beta, motor_state_modules[i]->torque_r, motor_state_modules[i]->torque_l);
+            Eigen::MatrixXd force_est = estimate_force(motor_state_modules[i]->theta, motor_state_modules[i]->beta,
+                                                       motor_state_modules[i]->torque_r, motor_state_modules[i]->torque_l);
 
             force_state_modules[i]->Fx = force_est(0, 0);
             force_state_modules[i]->Fy = force_est(1, 0);
-
-            // printf("TB = [%.4lf, %.4lf]\n", state->theta, state->beta);
-            // printf("Force_est = [%.4lf, %.4lf]\n", force_est(0, 0), force_est(1, 0));
         }
 
         force_state.header.seq = motor_state.header.seq;
