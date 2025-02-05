ac52a75df043753eeadfe6039c13ba86bd70880d
Eason <yisyuanshen@gmail.com>
2025-02-05 11:12:51 +0800

    Fix bug in force_estimation

--- src/corgi_force_estimation/src/force_estimation.cpp
+++ src/corgi_force_estimation/src/force_estimation.cpp
@@ -15,18 +15,28 @@ Eigen::MatrixXd calculate_P_poly(int rim, double alpha){
     for (int i=0; i<8; i++){
         H_l_coef(0, i) = H_x_coef[i];
         H_l_coef(1, i) = H_y_coef[i];
+
         U_l_coef(0, i) = U_x_coef[i];
         U_l_coef(1, i) = U_y_coef[i];
+
+        U_r_coef(0, i) = -U_x_coef[i];
+        U_r_coef(1, i) = U_y_coef[i];
+        
+        L_l_coef(0, i) = U_x_coef[i];
+        L_l_coef(1, i) = U_y_coef[i];
+        
+        L_r_coef(0, i) = -U_x_coef[i];
+        L_r_coef(1, i) = U_y_coef[i];
+        
         F_l_coef(0, i) = F_x_coef[i];
         F_l_coef(1, i) = F_y_coef[i];
+        
+        F_r_coef(0, i) = -F_x_coef[i];
+        F_r_coef(1, i) = F_y_coef[i];
+        
         G_coef(0, i) = 0;
         G_coef(1, i) = G_y_coef[i];
     }
-
-    L_r_coef = L_l_coef * (-1, 1);
-    F_r_coef = F_l_coef * (-1, 1);
-    U_r_coef = U_l_coef * (-1, 1);
-
 
     Eigen::MatrixXd P_poly(2, 8);
 
