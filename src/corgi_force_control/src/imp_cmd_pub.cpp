a9eade8ebac16cb8e63dbeb0ba02d98808f86b67
Eason <yisyuanshen@gmail.com>
2025-02-26 16:16:36 +0800

    Real robot test

--- src/corgi_force_control/src/imp_cmd_pub.cpp
+++ src/corgi_force_control/src/imp_cmd_pub.cpp
@@ -31,14 +31,14 @@ int main(int argc, char **argv) {
     };
 
     double M = 0;
-    double K = 6000;
-    double B = 80;
+    double K = 3000;
+    double B = 30;
 
     for (auto& cmd : imp_cmd_modules){
         cmd->theta = 17/180.0*M_PI;
         cmd->beta = 0/180.0*M_PI;
         cmd->Fx = 0;
-        cmd->Fy = 0;
+        cmd->Fy = -50;
         cmd->Mx = M;
         cmd->My = M;
         cmd->Bx = B;
@@ -53,59 +53,52 @@ int main(int argc, char **argv) {
         if (trigger){
             int loop_count = 0;
             while (ros::ok()) {
-                if (loop_count < 1000) {
+                if (loop_count < 10000) {
                     
                 }
 
-                else if (loop_count < 2000) {
+                else if (loop_count < 12000) {
                     imp_cmd_modules[0]->theta += 43/1000.0/180.0*M_PI;
-                    // imp_cmd_modules[1]->theta += 43/1000.0/180.0*M_PI;
-                    // imp_cmd_modules[2]->theta += 43/1000.0/180.0*M_PI;
-                    // imp_cmd_modules[3]->theta += 43/1000.0/180.0*M_PI;
+                    imp_cmd_modules[1]->theta += 43/1000.0/180.0*M_PI;
+                    imp_cmd_modules[2]->theta += 43/1000.0/180.0*M_PI;
+                    imp_cmd_modules[3]->theta += 43/1000.0/180.0*M_PI;
                     // imp_cmd_modules[0]->beta += 90/1000.0/180.0*M_PI;
                     // imp_cmd_modules[1]->beta += 90/1000.0/180.0*M_PI;
                     // imp_cmd_modules[2]->beta += 90/1000.0/180.0*M_PI;
                     // imp_cmd_modules[3]->beta += 90/1000.0/180.0*M_PI;
                 }
 
-                else if (loop_count < 3000) {
+                else if (loop_count < 13000) {
 
                 }
 
-                else if (loop_count < 4000) {
-                    imp_cmd_modules[0]->theta = (60-20*sin((loop_count-3000)/200.0*M_PI))/180.0*M_PI;
+                else if (loop_count < 14000) {
+                    imp_cmd_modules[0]->Fy = -20;
+                    imp_cmd_modules[1]->Fy = -80;
+                    imp_cmd_modules[2]->Fy = -20;
+                    imp_cmd_modules[3]->Fy = -80;
                 }
 
-                else if (loop_count < 5000) {
+                else if (loop_count < 15000) {
 
                 }
 
-                else if (loop_count < 6000) {
-                    imp_cmd_modules[0]->Fy = -60 + 20*sin((loop_count-5000)/50.0*M_PI);
-                    // imp_cmd_modules[0]->Fy = -50;
-                    // imp_cmd_modules[1]->Fy = -50;
-                    // imp_cmd_modules[2]->Fy = -50;
-                    // imp_cmd_modules[3]->Fy = -50;
-
-                    // for (int i=0; i<4; i++){
-                    //     imp_cmd_modules[i]->Kx = 300;
-                    //     imp_cmd_modules[i]->Ky = 300;
-                    //     imp_cmd_modules[i]->Bx = 5;
-                    //     imp_cmd_modules[i]->By = 5;
-                    // }
+                else if (loop_count < 16000) {
+                    imp_cmd_modules[0]->Fy = -80;
+                    imp_cmd_modules[1]->Fy = -20;
+                    imp_cmd_modules[2]->Fy = -80;
+                    imp_cmd_modules[3]->Fy = -20;
                 }
 
-                else if (loop_count < 7000) {
-                    // for (int i=0; i<4; i++){
-                    //     imp_cmd_modules[i]->Kx += 2.7;
-                    //     imp_cmd_modules[i]->Ky += 2.7;
-                    //     imp_cmd_modules[i]->Bx += 0.045;
-                    //     imp_cmd_modules[i]->By += 0.045;
-                    // }
+                else if (loop_count < 17000) {
+
                 }
 
                 else {
-                    
+                    imp_cmd_modules[0]->Fy = -50;
+                    imp_cmd_modules[1]->Fy = -50;
+                    imp_cmd_modules[2]->Fy = -50;
+                    imp_cmd_modules[3]->Fy = -50;
                 }
 
                 imp_cmd.header.seq = loop_count;
