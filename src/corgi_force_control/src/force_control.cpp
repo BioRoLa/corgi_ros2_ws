c640be158fab74cb7148105c5c1bf6dbc0c56698
Eason <yisyuanshen@gmail.com>
2025-03-21 22:54:53 +0800

    Use average values and add kt

--- src/corgi_force_control/src/force_control.cpp
+++ src/corgi_force_control/src/force_control.cpp
@@ -271,16 +271,16 @@ int main(int argc, char **argv) {
 
         // static friction　compensation
         // avg: 0.429, 0.272, 0.326, 0.267, 0.353, 0.288, 0.306, 0.236
-        // 95 percentile: 0.568, 0.334, 0.370, 0.330, 0.440, 0.356, 0.330, 0.359
-        motor_cmd_modules[0]->torque_r += 0.568 * Eigen::numext::sign(motor_cmd_modules[0]->torque_r);
-        motor_cmd_modules[0]->torque_l += 0.334 * Eigen::numext::sign(motor_cmd_modules[0]->torque_l);
-        motor_cmd_modules[1]->torque_r += 0.370 * Eigen::numext::sign(motor_cmd_modules[1]->torque_r);
-        motor_cmd_modules[1]->torque_l += 0.330 * Eigen::numext::sign(motor_cmd_modules[1]->torque_l);
-        motor_cmd_modules[2]->torque_r += 0.440 * Eigen::numext::sign(motor_cmd_modules[2]->torque_r);
-        motor_cmd_modules[2]->torque_l += 0.356 * Eigen::numext::sign(motor_cmd_modules[2]->torque_l);
-        motor_cmd_modules[3]->torque_r += 0.330 * Eigen::numext::sign(motor_cmd_modules[3]->torque_r);
-        motor_cmd_modules[3]->torque_l += 0.359 * Eigen::numext::sign(motor_cmd_modules[3]->torque_l);
-
+        // kt: 2.359, 2.499, 2.227, 2.309, 2.137, 2.531, 2.224, 2.612
+        motor_cmd_modules[0]->torque_r += 0.429 * Eigen::numext::sign(motor_cmd_modules[0]->torque_r) * 2.359;
+        motor_cmd_modules[0]->torque_l += 0.272 * Eigen::numext::sign(motor_cmd_modules[0]->torque_l) * 2.499;
+        motor_cmd_modules[1]->torque_r += 0.326 * Eigen::numext::sign(motor_cmd_modules[1]->torque_r) * 2.227;
+        motor_cmd_modules[1]->torque_l += 0.267 * Eigen::numext::sign(motor_cmd_modules[1]->torque_l) * 2.309;
+        motor_cmd_modules[2]->torque_r += 0.353 * Eigen::numext::sign(motor_cmd_modules[2]->torque_r) * 2.137;
+        motor_cmd_modules[2]->torque_l += 0.288 * Eigen::numext::sign(motor_cmd_modules[2]->torque_l) * 2.531;
+        motor_cmd_modules[3]->torque_r += 0.306 * Eigen::numext::sign(motor_cmd_modules[3]->torque_r) * 2.224;
+        motor_cmd_modules[3]->torque_l += 0.236 * Eigen::numext::sign(motor_cmd_modules[3]->torque_l) * 2.612;
+        
         // std::cout << "= = = = = = = = = = =" << std::endl << std::endl;
 
         motor_cmd.header.seq = loop_count;
