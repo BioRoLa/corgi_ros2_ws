ce6d4eba6435f7fec2b961498632882827b15866
Yatinghsu000627 <997726tina@gmail.com>
2025-03-23 15:43:30 +0800

    combine fsm_0

--- src/corgi_gait_selector/include/Simple_fsm.hpp
+++ src/corgi_gait_selector/include/Simple_fsm.hpp
@@ -1,28 +1,206 @@
 #ifndef SIMPLE_FSM_HPP
 #define SIMPLE_FSM_HPP
 
-
-#include <string>
 #include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <array>
+#include <vector>
+#include <cmath>
+#include <random>
+#include <iomanip>
 
 #include <Eigen/Dense>
 #include <Eigen/Geometry>
-#include "ros/ros.h"
 
+#include "ros/ros.h"
 #include "corgi_msgs/MotorCmdStamped.h"
 #include "corgi_msgs/MotorStateStamped.h"
 
-#include "corgi_msgs/FsmCmdStamped.h"
-#include "corgi_msgs/FsmStateStamped.h"
-
-#include "corgi_msgs/TriggerStamped.h"
-
+#include "leg_model.hpp"
 
 enum class Gait {
-    WHEEL, //1
-    LEG,   //2
-    WLW,   //3
-    TRANSFORM
+    WHEEL = 0,
+    LEG = 1,   
+    WLW = 2,   
+    TRANSFORM = 3
 };
+class GaitSelector {
+    public:
+        GaitSelector(   ros::NodeHandle& nh,
+                        bool sim=true,
+                        double CoM_bias=0.0,
+                        int pub_rate=1000,
+                        double BL=0.444,
+                        double BW=0.4,
+                        double BH=0.2);
+        ~GaitSelector();
 
+        ros::Subscriber motor_state_sub_;
+        ros::Publisher motor_cmd_pub_; 
+        ros::Rate* rate_ptr;
+        std::random_device rd;                     
+        std::mt19937 rng;                          
+        std::uniform_int_distribution<int> dist;
+    /*    State or cmd messages      */ 
+        corgi_msgs::MotorCmdStamped motor_cmd;
+        corgi_msgs::MotorStateStamped motor_state;
+        std::vector<corgi_msgs::MotorCmd*> motor_cmd_modules = {
+            &motor_cmd.module_a,
+            &motor_cmd.module_b,
+            &motor_cmd.module_c,
+            &motor_cmd.module_d
+        };
+        std::vector<corgi_msgs::MotorState*> motor_state_modules = {
+            &motor_state.module_a,
+            &motor_state.module_b,
+            &motor_state.module_c,
+            &motor_state.module_d
+        };
+        void motor_state_cb(const corgi_msgs::MotorStateStamped state){
+            motor_state = state;
+        }               
+
+        /*     Cooperate variables      */ 
+        LegModel leg_model;
+        int pub_rate;
+        const double CoM_bias;
+        const double BL;
+        const double BW;
+        const double BH;
+
+        std::array<double, 4> duty;
+        std::array<int, 4> swing_phase = {0, 0, 0, 0};
+        double swing_time;   // ratio
+        double velocity;     // m/s
+        double stand_height; // m
+        double step_length;  // m
+        double step_height;  // m
+
+        double eta[4][2];
+        double next_eta[4][2];
+        std::array<std::array<double, 2>, 4> foothold;
+        std::array<std::array<double, 2>, 4> next_foothold;
+        std::array<std::array<double, 2>, 4> body;
+        std::array<std::array<double, 2>, 4> next_body;
+        std::array<std::array<double, 2>, 4> hip;
+        std::array<std::array<double, 2>, 4> next_hip;
+
+        /*     Cooperate functions      */ 
+        // ex.send, pub, 
+
+
+    private:
+        /*     Gait Selector     */ 
+        Gait currentGait;
+        Gait newGait;
+        void changeGait(const std::string& command) {
+            newGait = currentGait;
+
+            if (command == "1") {
+                newGait = Gait::WHEEL;
+            } else if (command == "2") {
+                newGait = Gait::LEG;
+            } else if (command == "3") {
+                newGait = Gait::WLW;
+            } else if (command[0] == 'v') {
+                std::string velocity = command.substr(1);
+                std::cout << "Setting velocity to " << velocity << std::endl;
+                return;
+            } else if (command[0] == 'l') {
+                std::string stepLength = command.substr(1);
+                std::cout << "Setting step length to " << stepLength << std::endl;
+                return;
+            } else if (command[0] == 'h') {
+                std::string standHeight = command.substr(1);
+                std::cout << "Setting stand height to " << standHeight << std::endl;
+                return;
+            } else if (command[0] == 'j') {
+                std::string liftHeight = command.substr(1);
+                std::cout << "Setting lift height to " << liftHeight << std::endl;
+                return;
+            } else {
+                std::cerr << "Unknown command: " << command << std::endl;
+                return;
+            }
+
+            if (newGait != currentGait) {
+                printCurrentGait();
+                Transform();  
+                currentGait = newGait; 
+          }
+        }
+        void printCurrentGait() const {
+            switch (currentGait) {
+            case Gait::WHEEL:
+                std::cout << "Current gait: WHEEL" << std::endl;
+                break;
+            case Gait::LEG:
+                std::cout << "Current gait: LEG" << std::endl;
+                break;
+            case Gait::WLW:
+                std::cout << "Current gait: WLW" << std::endl;
+                break;
+            case Gait::TRANSFORM:
+                std::cout << "Current gait: TRANSFORM" << std::endl;
+                break;
+            default:
+                std::cerr << "Unknown gait" << std::endl;
+                break;
+            }
+        }
+        void Transform() {
+            switch (currentGait)
+            {
+            case Gait::WHEEL:
+                currentGait = Gait::TRANSFORM;
+                printCurrentGait();
+                if(newGait == Gait::LEG) {
+                    std::cout << "Transforming from WHEEL to LEG" << std::endl;
+                } 
+                else if(newGait == Gait::WLW) {
+                    std::cout << "Transforming from WHEEL to WLW" << std::endl;
+                }
+                else{
+                    std::cerr << "Unknown transform gait" << std::endl;
+                }
+            break;
+            case Gait::LEG:
+                currentGait = Gait::TRANSFORM;
+                printCurrentGait();
+                if(newGait == Gait::WHEEL) {
+                    std::cout << "Transforming from LEG to WHEEL" << std::endl;
+                } 
+                else if(newGait == Gait::WLW) {
+                    std::cout << "Transforming from LEG to WLW" << std::endl;
+                } 
+                else{
+                    std::cerr << "Unknown transform gait" << std::endl;
+                }
+                break;
+            case Gait::WLW:
+                currentGait = Gait::TRANSFORM;
+                printCurrentGait();
+                if(newGait == Gait::WHEEL) {
+                    std::cout << "Transforming from WLW to WHEEL" << std::endl;
+                } 
+                else if(newGait == Gait::LEG) {
+                    std::cout << "Transforming from WLW to LEG" << std::endl;
+                } 
+                else{
+                    std::cerr << "Unknown transform gait" << std::endl;
+                }
+                break;
+            
+            default:
+                break;
+            }
+            
+            std::cout << "Transforming..." << std::endl;
+        }
+    };
 #endif
+
+// gaitselextor setup add nodehandler and all initialize
+// CoM_bias to 2D array(current 1D)
--- src/corgi_gait_selector/src/Simple_fsm.cpp
+++ src/corgi_gait_selector/src/Simple_fsm.cpp
@@ -1,138 +1,50 @@
 #include "Simple_fsm.hpp"
 
-class GaitSelector {
-    public:
-        GaitSelector() : currentGait(Gait::WHEEL) {}
-        void changeGait(const std::string& command) {
-            newGait = currentGait;
+GaitSelector::GaitSelector( ros::NodeHandle& nh, 
+                            bool sim, 
+                            double CoM_bias, 
+                            int pub_rate, 
+                            double BL, 
+                            double BW, 
+                            double BH): 
+    leg_model(sim), 
+    CoM_bias(CoM_bias), 
+    BL(BL), 
+    BW(BW), 
+    BH(BH), 
+    pub_rate(pub_rate),
+    rng(rd()), 
+    dist(0, 359),
+    currentGait(Gait::WHEEL)
+{
+    motor_state_sub_ = nh.subscribe("/motor/state", 1000, &GaitSelector::motor_state_cb, this);
+    motor_cmd_pub_ = nh.advertise<corgi_msgs::MotorCmdStamped>("/motor/command", pub_rate);
+    rate_ptr = new ros::Rate(pub_rate);
+}
 
-            if (command == "1") {
-                newGait = Gait::WHEEL;
-            } else if (command == "2") {
-                newGait = Gait::LEG;
-            } else if (command == "3") {
-                newGait = Gait::WLW;
-            } else if (command[0] == 'v') {
-                std::string velocity = command.substr(1);
-                std::cout << "Setting velocity to " << velocity << std::endl;
-                return;
-            } else if (command[0] == 'l') {
-                std::string stepLength = command.substr(1);
-                std::cout << "Setting step length to " << stepLength << std::endl;
-                return;
-            } else if (command[0] == 'h') {
-                std::string standHeight = command.substr(1);
-                std::cout << "Setting stand height to " << standHeight << std::endl;
-                return;
-            } else if (command[0] == 'j') {
-                std::string liftHeight = command.substr(1);
-                std::cout << "Setting lift height to " << liftHeight << std::endl;
-                return;
-            } else {
-                std::cerr << "Unknown command: " << command << std::endl;
-                return;
-            }
-
-            if (newGait != currentGait) {
-                printCurrentGait();
-                Transform();  
-                currentGait = newGait; 
-          }
-        }
-    private:
-        Gait currentGait;
-        Gait newGait;
-        void printCurrentGait() const {
-            switch (currentGait) {
-            case Gait::WHEEL:
-                std::cout << "Current gait: WHEEL" << std::endl;
-                break;
-            case Gait::LEG:
-                std::cout << "Current gait: LEG" << std::endl;
-                break;
-            case Gait::WLW:
-                std::cout << "Current gait: WLW" << std::endl;
-                break;
-            case Gait::TRANSFORM:
-                std::cout << "Current gait: TRANSFORM" << std::endl;
-                break;
-            default:
-                std::cerr << "Unknown gait" << std::endl;
-                break;
-            }
-        }
-        void Transform() {
-            switch (currentGait)
-            {
-            case Gait::WHEEL:
-                currentGait = Gait::TRANSFORM;
-                printCurrentGait();
-                if(newGait == Gait::LEG) {
-                    std::cout << "Transforming from WHEEL to LEG" << std::endl;
-                } 
-                else if(newGait == Gait::WLW) {
-                    std::cout << "Transforming from WHEEL to WLW" << std::endl;
-                }
-                else{
-                    std::cerr << "Unknown transform gait" << std::endl;
-                }
-            break;
-            case Gait::LEG:
-                currentGait = Gait::TRANSFORM;
-                printCurrentGait();
-                if(newGait == Gait::WHEEL) {
-                    std::cout << "Transforming from LEG to WHEEL" << std::endl;
-                } 
-                else if(newGait == Gait::WLW) {
-                    std::cout << "Transforming from LEG to WLW" << std::endl;
-                } 
-                else{
-                    std::cerr << "Unknown transform gait" << std::endl;
-                }
-                break;
-            case Gait::WLW:
-                currentGait = Gait::TRANSFORM;
-                printCurrentGait();
-                if(newGait == Gait::WHEEL) {
-                    std::cout << "Transforming from WLW to WHEEL" << std::endl;
-                } 
-                else if(newGait == Gait::LEG) {
-                    std::cout << "Transforming from WLW to LEG" << std::endl;
-                } 
-                else{
-                    std::cerr << "Unknown transform gait" << std::endl;
-                }
-                break;
-            
-            default:
-                break;
-            }
-            
-            std::cout << "Transforming..." << std::endl;
-        }
-
-        
-    };
-
+GaitSelector::~GaitSelector() {
+    delete rate_ptr;
+    rate_ptr = nullptr;
+}
 
 int main(int argc, char **argv){
-    GaitSelector gaitSelector;
-    std::cout << "Starting gait selector" << std::endl;
-    std::cout << "changeGait(2)" << std::endl;
-    gaitSelector.changeGait("2");
-    std::cout << "changeGait(3)" << std::endl;
-    gaitSelector.changeGait("3");
-    std::cout << "changeGait(1)" << std::endl;
-    gaitSelector.changeGait("1");
-    std::cout << "changeGait(v0.5)" << std::endl;
-    gaitSelector.changeGait("v0.5");
-    std::cout << "changeGait(l0.3)" << std::endl;
-    gaitSelector.changeGait("l0.3");
-    std::cout << "changeGait(h0.2)" << std::endl;
-    gaitSelector.changeGait("h0.2");
-    std::cout << "changeGait(j0.1)" << std::endl;
-    gaitSelector.changeGait("j0.1");
-    std::cout << "changeGait(4)" << std::endl;
-    gaitSelector.changeGait("4");
+    ROS_INFO("Simple FSM Control\n");
+    ros::init(argc, argv, "corgi_gait_selector");
+    ros::NodeHandle nh;
+
+    //  Start an async spinner to run in parallel.
+    ros::AsyncSpinner spinner(1);
+    spinner.start();    
+
+    bool sim = true;
+    double CoM_bias = 0.0;
+    int pub_rate = 1000;
+
+
+    GaitSelector gaitSelector(nh, sim, CoM_bias, pub_rate);
     return 0;
 }
+
+/*    Initialize of each mode   */ 
+
+/*     Gait Selector Setting    */ 
