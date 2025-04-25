d5f3653e8fb38e8a4010fba9f44dec0cae1e463a
kyle1548 <kyle9296@gmail.com>
2025-04-25 13:52:34 +0800

    test ransac

--- src/corgi_stair/src/plane_segmentation_node.cpp
+++ src/corgi_stair/src/plane_segmentation_node.cpp
@@ -38,40 +38,83 @@ void cloudCallback(const sensor_msgs::PointCloud2ConstPtr& input)
     pass.setFilterLimits(-0.5, 1.0);
     pass.filter(*cloud);
 
-    // Estimate normals
-    pcl::PointCloud<pcl::Normal>::Ptr normals(new pcl::PointCloud<pcl::Normal>);
-    pcl::IntegralImageNormalEstimation<PointT, pcl::Normal> ne;
-    ne.setNormalEstimationMethod(ne.AVERAGE_3D_GRADIENT);
-    ne.setMaxDepthChangeFactor(0.05f);
-    ne.setNormalSmoothingSize(5.0f);
-    ne.setInputCloud(cloud);
-    ne.compute(*normals);
+    // // Estimate normals
+    // pcl::PointCloud<pcl::Normal>::Ptr normals(new pcl::PointCloud<pcl::Normal>);
+    // pcl::IntegralImageNormalEstimation<PointT, pcl::Normal> ne;
+    // ne.setNormalEstimationMethod(ne.AVERAGE_3D_GRADIENT);
+    // ne.setMaxDepthChangeFactor(0.05f);
+    // ne.setNormalSmoothingSize(15.0f);
+    // ne.setInputCloud(cloud);
+    // ne.compute(*normals);
 
-    // Plane segmentation
-    pcl::OrganizedMultiPlaneSegmentation<PointT, pcl::Normal, pcl::Label> mps;
-    mps.setMinInliers(50);
-    mps.setAngularThreshold(0.017453 * 20.0); // 20 degrees in radians
-    mps.setDistanceThreshold(0.02);          // 2cm
-    mps.setInputCloud(cloud);
-    mps.setInputNormals(normals);
+    // // Plane segmentation
+    // pcl::OrganizedMultiPlaneSegmentation<PointT, pcl::Normal, pcl::Label> mps;
+    // mps.setMinInliers(50);
+    // mps.setAngularThreshold(0.017453 * 20.0); // 20 degrees in radians
+    // mps.setDistanceThreshold(0.02);          // 2cm
+    // mps.setInputCloud(cloud);
+    // mps.setInputNormals(normals);
 
-    std::vector<pcl::PlanarRegion<PointT>, Eigen::aligned_allocator<pcl::PlanarRegion<PointT>>> regions;
-    mps.segment(regions); // 原始版本僅需 regions，無需 refine
+    // std::vector<pcl::PlanarRegion<PointT>, Eigen::aligned_allocator<pcl::PlanarRegion<PointT>>> regions;
+    // mps.segment(regions); // 原始版本僅需 regions，無需 refine
 
-    pcl::PointCloud<PointT>::Ptr all_planes(new pcl::PointCloud<PointT>);
+    // pcl::PointCloud<PointT>::Ptr all_planes(new pcl::PointCloud<PointT>);
 
-    for (size_t i = 0; i < regions.size(); ++i)
+    // for (size_t i = 0; i < regions.size(); ++i)
+    // {
+    //     const pcl::PlanarRegion<PointT>& region = regions[i];
+    //     pcl::PointCloud<PointT> contour;
+    //     contour.points = region.getContour();
+    //     contour.width = contour.points.size();
+    //     contour.height = 1;
+    //     contour.is_dense = true;
+
+    //     *all_planes += contour; // 合併所有輪廓到一起
+    // }
+
+    pcl::PointCloud<pcl::PointXYZ>::Ptr working_cloud(new pcl::PointCloud<pcl::PointXYZ>);
+    *working_cloud = *cloud;
+
+    pcl::PointCloud<pcl::PointXYZ>::Ptr all_planes(new pcl::PointCloud<pcl::PointXYZ>);
+
+    int max_planes = 5;
+    float distance_threshold = 0.01;  // 1cm
+    int min_inliers = 100;
+
+    for (int i = 0; i < max_planes; ++i)
     {
-        const pcl::PlanarRegion<PointT>& region = regions[i];
-        pcl::PointCloud<PointT> contour;
-        contour.points = region.getContour();
-        contour.width = contour.points.size();
-        contour.height = 1;
-        contour.is_dense = true;
+        pcl::SACSegmentation<pcl::PointXYZ> seg;
+        pcl::PointIndices::Ptr inliers(new pcl::PointIndices);
+        pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients);
 
-        *all_planes += contour; // 合併所有輪廓到一起
+        seg.setOptimizeCoefficients(true);
+        seg.setModelType(pcl::SACMODEL_PLANE);
+        seg.setMethodType(pcl::SAC_RANSAC);
+        seg.setDistanceThreshold(distance_threshold);
+        seg.setInputCloud(working_cloud);
+        seg.segment(*inliers, *coefficients);
+
+        if (inliers->indices.size() < min_inliers)
+            break;
+
+        // 提取平面點
+        pcl::ExtractIndices<pcl::PointXYZ> extract;
+        pcl::PointCloud<pcl::PointXYZ>::Ptr plane(new pcl::PointCloud<pcl::PointXYZ>);
+        extract.setInputCloud(working_cloud);
+        extract.setIndices(inliers);
+        extract.setNegative(false);
+        extract.filter(*plane);
+
+        *all_planes += *plane;
+
+        // 移除平面點，繼續找下一個
+        extract.setNegative(true);
+        pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_f(new pcl::PointCloud<pcl::PointXYZ>);
+        extract.filter(*cloud_f);
+        working_cloud = cloud_f;
     }
 
+    
     // 發布結果
     for (auto& point : all_planes->points) {
         point.r = 255;
