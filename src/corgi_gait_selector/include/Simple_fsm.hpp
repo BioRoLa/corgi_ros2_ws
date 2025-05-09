fd515436c4bb35af607037cf33f2fb08a6f506e9
Eason <yisyuanshen@gmail.com>
2025-05-09 17:15:11 +0800

    Merge corgi_gait_selector from wlw branch

--- src/corgi_gait_selector/include/Simple_fsm.hpp
+++ src/corgi_gait_selector/include/Simple_fsm.hpp
@@ -17,6 +17,7 @@
 #include <algorithm>
 #include <Eigen/Dense>
 #include <Eigen/Geometry>
+#include <visualization_msgs/Marker.h>
 
 #include "ros/ros.h"
 #include "corgi_msgs/MotorCmdStamped.h"
@@ -46,6 +47,7 @@ class GaitSelector {
         
         ros::Subscriber motor_state_sub_;
         ros::Publisher motor_cmd_pub_; 
+        ros::Publisher marker_pub_;
         ros::Rate* rate_ptr;
         std::random_device rd;                     
         std::mt19937 rng;                          
@@ -72,7 +74,7 @@ class GaitSelector {
         static std::array<double, 4> duty;
         static std::array<int, 4> swing_phase;
         static double swing_time;   // ratio
-        static double velocity;     // m/s //wheel 有吃到？？？
+        static double velocity;     // m/s 
         static double stand_height; // m
         static double step_length;  // m
         static double step_height;  // m
@@ -81,22 +83,31 @@ class GaitSelector {
         static std::array<double, 4> next_step_length;
         static double new_step_length ;
 
+        static std::array<double, 4> current_shift;
+
         static double curvature;
 
         double dS;
         double incre_duty;
 
+        static int pub_time;
+        static int do_pub;
+        static int transfer_state;
+        static int transfer_sec;
+        static int wait_sec;
+
         static double relative_foothold[4][2];
         static double eta[4][2];
         static double next_eta[4][2];
-        static std::array<std::array<double, 2>, 4> foothold;
-        static std::array<std::array<double, 2>, 4> next_foothold;
-        static std::array<std::array<double, 2>, 4> body;
-        static std::array<std::array<double, 2>, 4> next_body;
-        static std::array<std::array<double, 2>, 4> hip;
-        static std::array<std::array<double, 2>, 4> next_hip;
+        static std::array<std::array<double, 3>, 4> foothold;
+        static std::array<std::array<double, 3>, 4> next_foothold;
+        static std::array<double, 3> body;
+        static std::array<double, 3> next_body;
+        static std::array<std::array<double, 3>, 4> hip;
+        static std::array<std::array<double, 3>, 4> next_hip;
 
         // For turning 
+        static int direction;
         static double outer_radius;
         static double inner_radius;
         static double diff_step_length;  // Differential step length 
@@ -112,10 +123,18 @@ class GaitSelector {
         void Transfer(int pub, int transfer_sec, int wait_sec);
         void Receive();
         // void changeGait(const std::string& command);
-
+        // void process_and_visualize();
+        // void publish_support_polygon(
+        //     const std::vector<Eigen::Vector3d>& pts,
+        //     const std::vector<int>& indices
+        // );
+        // void publish_com_marker();
+        // bool is_point_inside_polygon(
+        //     const std::vector<Eigen::Vector3d>& points,
+        //     const std::vector<int>& indices,
+        //     const Eigen::Vector2d& point_xy
+        // ) ;
     private:
-              
-        
         // void printCurrentGait() const;
         // void GaitTransform();
         std::vector<double> linspace(double start, double end, int num_steps);
