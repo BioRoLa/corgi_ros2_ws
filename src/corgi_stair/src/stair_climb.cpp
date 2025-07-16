4898905fb8cd775a2069d7b47a492df908e5e9d7
kyle1548 <kyle9296@gmail.com>
2025-07-16 17:23:17 +0800

    test

--- src/corgi_stair/src/stair_climb.cpp
+++ src/corgi_stair/src/stair_climb.cpp
@@ -415,7 +415,6 @@ bool StairClimb::move_CoM_stable() {    // return true if stable, false if not
                 //     front_height -= velocity[1] / rate;
                 } else {
                     front_height -= max_down;
-                    throw std::runtime_error("Check: leg 0.");
                     if (theta[0]*180/M_PI < 17.1) {
                         wheel_mode[0] = true; // enter wheel mode
                     }//end if
@@ -439,7 +438,6 @@ bool StairClimb::move_CoM_stable() {    // return true if stable, false if not
                 //     front_height -= velocity[1] / rate;
                 } else {
                     front_height -= max_down;
-                    throw std::runtime_error("Check: leg 1.");
                     if (theta[1]*180/M_PI < 17.1) {
                         wheel_mode[1] = true; // enter wheel mode
                     }//end if
