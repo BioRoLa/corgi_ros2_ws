0d2a4d1e576f1444c23bbf89d07829efa0c75e4b
Yu-An-Xiao <annexiao1007@gmail.com>
2026-02-26 09:42:00 +0000

    feat: add launch file for force control nodes

---
+++ src/corgi_force_control/launch/force_control.launch.py
@@ -0,0 +1,22 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    common_params = [{'use_sim_time': True}]
+
+    return LaunchDescription([
+
+        Node(
+            package='corgi_force_control',
+            executable='force_control_node',
+            name='force_control_node',
+            parameters=common_params
+        ),
+
+        Node(
+            package='corgi_force_control',
+            executable='exp_h24_node',
+            name='exp_h24_node',
+            parameters=common_params
+        )
+    ])
