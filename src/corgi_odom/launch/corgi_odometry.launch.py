120edacbdff441caf56e956fc17db8f0bbd7dd2d
BioRoLa <biorolab@gmail.com>
2026-03-03 23:31:51 +0800

    fix: add common parameters to nodes in corgi_odometry.launch.py

--- src/corgi_odom/launch/corgi_odometry.launch.py
+++ src/corgi_odom/launch/corgi_odometry.launch.py
@@ -2,17 +2,23 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
+    common_params = [{'use_sim_time': False}]
+
     return LaunchDescription([
+
         Node(
             package='corgi_odom',
             executable='corgi_odometry',
             name='corgi_odometry',
+            parameters=common_params,
             output='screen'
         ),
+
         Node(
             package='corgi_odom',
             executable='corgi_z_position',
             name='corgi_z_position',
+            parameters=common_params,
             output='screen'
         )
-    ])
+    ])  
