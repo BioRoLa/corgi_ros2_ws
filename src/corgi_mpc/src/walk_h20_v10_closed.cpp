d4020de1a65082bae95c269142c0df224a9b6af7
BioRoLa <biorolab@gmail.com>
2026-03-03 23:31:26 +0800

    fix: reduce subscription and publication queue sizes in walk_h20_v10_closed.cpp

--- src/corgi_mpc/src/walk_h20_v10_closed.cpp
+++ src/corgi_mpc/src/walk_h20_v10_closed.cpp
@@ -73,16 +73,16 @@ int main(int argc, char **argv) {
     mpc.load_config();
     mpc.target_loop = 2250;
 
-    auto imp_cmd_pub = node->create_publisher<corgi_msgs::msg::ImpedanceCmdStamped>("impedance/command", 1000);
-    auto contact_pub = node->create_publisher<corgi_msgs::msg::ContactStateStamped>("odometry/contact", 1000);
-    auto trigger_sub = node->create_subscription<corgi_msgs::msg::TriggerStamped>("trigger", 1000, trigger_cb);
-    auto sim_data_sub = node->create_subscription<corgi_msgs::msg::SimDataStamped>("sim/data", 1000, sim_data_cb);
-    auto force_state_sub = node->create_subscription<corgi_msgs::msg::ForceStateStamped>("force/state", 1000, force_state_cb);
-    auto motor_state_sub = node->create_subscription<corgi_msgs::msg::MotorStateStamped>("motor/state", 1000, motor_state_cb);
-    auto odom_pos_sub = node->create_subscription<geometry_msgs::msg::Vector3>("odometry/position", 1000, odom_pos_cb);
-    auto odom_vel_sub = node->create_subscription<geometry_msgs::msg::Vector3>("odometry/velocity", 1000, odom_vel_cb);
-    auto odom_z_sub = node->create_subscription<std_msgs::msg::Float64>("odometry/z_position_hip", 1000, odom_z_cb);
-    auto imu_sub = node->create_subscription<corgi_msgs::msg::ImuStamped>("imu", 1000, imu_cb);
+    auto imp_cmd_pub = node->create_publisher<corgi_msgs::msg::ImpedanceCmdStamped>("impedance/command", 10);
+    auto contact_pub = node->create_publisher<corgi_msgs::msg::ContactStateStamped>("odometry/contact", 10);
+    auto trigger_sub = node->create_subscription<corgi_msgs::msg::TriggerStamped>("trigger", 10, trigger_cb);
+    auto sim_data_sub = node->create_subscription<corgi_msgs::msg::SimDataStamped>("sim/data", 10, sim_data_cb);
+    auto force_state_sub = node->create_subscription<corgi_msgs::msg::ForceStateStamped>("force/state", 10, force_state_cb);
+    auto motor_state_sub = node->create_subscription<corgi_msgs::msg::MotorStateStamped>("motor/state", 10, motor_state_cb);
+    auto odom_pos_sub = node->create_subscription<geometry_msgs::msg::Vector3>("odometry/position", 10, odom_pos_cb);
+    auto odom_vel_sub = node->create_subscription<geometry_msgs::msg::Vector3>("odometry/velocity", 10, odom_vel_cb);
+    auto odom_z_sub = node->create_subscription<std_msgs::msg::Float64>("odometry/z_position_hip", 10, odom_z_cb);
+    auto imu_sub = node->create_subscription<corgi_msgs::msg::ImuStamped>("imu", 10, imu_cb);
 
     rclcpp::Duration period(0, 1000000000.0 / mpc.freq); // Convert Hz to nanoseconds
     rclcpp::Time next_time = node->now();
