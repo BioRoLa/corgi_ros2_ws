d49df4e5edf1e25c0aa7a71facb9ea626b85b01b
rumsey000 <rumseyprocess000@gmail.com>
2026-04-07 03:45:00 +0800

    feat: add launch file for force_control_3d_node and implement exp_h24_3d_node in CMakeLists.txt

---
+++ src/corgi_force_control/launch/force_control_3d.launch.py
@@ -0,0 +1,21 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    common_params = [{'use_sim_time': True}]
+
+    return LaunchDescription([
+        Node(
+            package='corgi_force_estimation',
+            executable='force_estimation_node',
+            name='force_estimation_node',
+            parameters=common_params
+        ),
+        Node(
+            package='corgi_force_control',
+            executable='force_control_3d_node',
+            name='force_control_3d_node',
+            parameters=common_params
+        )
+    ])
