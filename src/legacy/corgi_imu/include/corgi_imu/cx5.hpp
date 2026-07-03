c1d637ec5c0becaed06c64ad781795e35cfaecce
changtsailin <angel.tl314@gmail.com>
2026-07-03 17:44:25 +0800

    fix(CX5_AHRS): enable sensor and filter data streams in device initialization

--- src/legacy/corgi_imu/include/corgi_imu/cx5.hpp
+++ src/legacy/corgi_imu/include/corgi_imu/cx5.hpp
@@ -188,8 +188,11 @@ class CX5_AHRS {
             device->registerExtractor(sensor_data_handlers[2], &raw_accel);
             device->registerExtractor(sensor_data_handlers[3], &g);
 
-            if(commands_base::resume(*device) != CmdResult::ACK_OK)
-                throw std::runtime_error("ERROR: Could not resume the device!");
+            if(commands_3dm::writeDatastreamControl(*device, data_sensor::DESCRIPTOR_SET, true) != CmdResult::ACK_OK)
+                throw std::runtime_error("ERROR: Could not enable sensor data stream!");
+
+            if(commands_3dm::writeDatastreamControl(*device, data_filter::DESCRIPTOR_SET, true) != CmdResult::ACK_OK)
+                throw std::runtime_error("ERROR: Could not enable filter data stream!");
 
             // bool filter_state_ahrs = false;
             Eigen::Matrix3f rot;
